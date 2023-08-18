d26d4ab52746ebc57a0f7a95246c703037c2a58b
Guilherme I. Schmidt T <80853300+Dufyz@users.noreply.github.com>
2023-08-18 10:35:00 -0300

    Funcionando completo; falta executável

--- browser.py
+++ browser.py
@@ -4,20 +4,13 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from headers import LINK_MAIN
 
-
 # Criando folders que serão utilizados para salvar os arquivos
 PATH_DESKTOP = str(Path.home()) + "\Desktop"
-
-# Cursores driver Browser
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--kiosk-printing')
-# chrome_options.add_argument('--no-sandbox')
-# browser = webdriver.Chrome(r"chromedriver.exe", options=chrome_options)
 
 browser = webdriver.Chrome()
 
 browser.headless = True 
-wait = WebDriverWait(browser, 10)
+wait = WebDriverWait(browser, 3)
 
 def getBrowserDriver():
     browser.get(LINK_MAIN)
